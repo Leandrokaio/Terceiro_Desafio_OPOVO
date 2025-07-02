1c6487dfa8ec04cd9bbb79cf4a06921e543f0ae4
Kaio Leandro Eloi Façanha <dig.kaiofacanha@opovo.local>
2025-07-02 16:59:28 -0300

    feat: Adicionando validação ao CEP

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
-// Constantes universais 
-const addressForm = document.querySelector("#form-container")
+// Constantes universais
+const addressForm = document.getElementById("form");
 const cepInput = document.querySelector("#cep-input");
 const cepValue = document.querySelector("#cep-info");
 const locationInput = document.querySelector("#location");
@@ -10,24 +10,38 @@ const stateInput = document.querySelector("#state");
 const neighborhoodInput = document.querySelector("#neighborhood");
 const btnInput = document.querySelector("#btnInput");
 
+function validaCep(cep) {
+  const cepRegex = /^\d{5}\d{3}$/;
 
-const consultarCep = async (cep) =>{
-    const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
-
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(data =>{
-        cepValue.value = data.cep;
-        locationInput.value = data.localidade;
-        neighborhoodInput.value = data.bairro;
-        addressInput.value = data.logradouro;
-        ufInput.value = data.uf;
-        complementInput.value = data.complemento
-        stateInput.value = data.estado;
-    })
-
+  if (!cepRegex.test(cep)) {
+    return false;
+  }
+  return true;
 }
 
+addressForm.addEventListener("submit", () => {
+  const cepValueInput = cepInput.value;
+  const cepValido = validaCep(cepValueInput);
 
+  if (!cepValido) {
+    alert("CEP inválido");
+  }
 
+  consultarCep(cepValueInput);
+});
 
+const consultarCep = async (cep) => {
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      cepValue.value = data.cep;
+      locationInput.value = data.localidade;
+      neighborhoodInput.value = data.bairro;
+      addressInput.value = data.logradouro;
+      ufInput.value = data.uf;
+      complementInput.value = data.complemento;
+      stateInput.value = data.estado;
+    });
+};
